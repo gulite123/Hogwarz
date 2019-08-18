8dd3cd83e2c937a8fcd9072a5debc74435372524
gulite123 <zhongxuren@163.com>
2019-08-19 01:10:50 +0800

    更新0819

--- APITest/test_requests.py
+++ APITest/test_requests.py
@@ -14,3 +14,14 @@ class TestRequest(object):
 
         #将 Python 对象编码成 JSON 字符串
         logging.info(json.dumps(r.json(),indent=2))
+
+    def test_post(self):
+        r=requests.post(self.url,
+                        params={"a":1,"b":"string content"},
+                        proxies={"https":"127.0.0.1:7888"},
+                        #伪造本地证书
+                        verify=False)
+        logging.info(r)
+        #logging.info(r.text)
+        #序列化json格式
+        logging.info(json.dumps(r.json(),indent=2))
