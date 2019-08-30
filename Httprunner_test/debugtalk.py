5fdbc2401873813fa6d4dfc2d7fce2873aa8de26
gulite123 <zhongxuren@163.com>
2019-08-30 19:25:28 +0800

    20190830

--- Httprunner_test/debugtalk.py
+++ Httprunner_test/debugtalk.py
@@ -3,3 +3,19 @@ import time
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+
+def get_timestamp():
+    now_time=time.strftime('%Y_%m_%d_%H_%M_%S')
+    return now_time
+    #return str(int(time.time()*1000))
+
+def print_documentId(doc_id):
+    print("documentId = {}".format(doc_id))
+
+def get_base_url(env_type= "prod"):
+    if env_type=="prod":
+        return "https://mubu.com"
+
+    else:
+        return "https://test.mubu.com"
