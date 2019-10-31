19dda7777f1cf93bb0bb60216c6f2a2daa311918
gulite123 <zhongxuren@163.com>
2019-10-31 10:59:15 +0800

    第一次提交

--- APITest/test_requests.py
+++ APITest/test_requests.py
@@ -25,3 +25,22 @@ class TestRequest(object):
         #logging.info(r.text)
         #序列化json格式1
         logging.info(json.dumps(r.json(),indent=2))
+
+    def test_cookies(self):
+        r = requests.get("http://47.95.238.18:8090/cookies",
+                         cookies={"a": "1", "b": "string corntent"})
+        logging.info(r.text)
+
+    def test_xueqiu(self):
+        url = "https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?"
+        r = requests.get(url,
+                         params={"category": "1"},
+                         headers={'User-Agent': 'Xueqiu Android 11.19'},
+                         cookies={"u": "3446260779", "xq_a_token": "5806a70c6bc5d5fb2b00978aeb1895532fffe502",
+                                  }
+
+                         # verify=False
+                         )
+
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(), indent=2))
--- API_Testing/test01.py
+++ API_Testing/test01.py
@@ -20,4 +20,29 @@ class TestRequests(object):
         logging.info(r.url)
         logging.info(r.text)
 
+    def test_cookies(self):
+        r=requests.get("http://47.95.238.18:8090/cookies",
+                       cookies={"a":"1","b":"string corntent"})
+        logging.info(r.text)
+
+
+    def test_xueqiu(self):
+        url="https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?"
+        r=requests.get(url,
+                        params={"category": "1"},
+                        headers={'User-Agent': 'Xueqiu Android 11.19'},
+                        cookies={"u":"3446260779","xq_a_token":"5806a70c6bc5d5fb2b00978aeb1895532fffe502",
+                                 }
+
+
+
+                       #verify=False
+                       )
+
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(),indent=2))
+
+
+
+
 
